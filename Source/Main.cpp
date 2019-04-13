309d107cd698df8771f722e986161981f9382046
Christian <christian173p@gmail.com>
2019-04-13 20:57:35 +0100

    Multihilo

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,8 +3,12 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 
+#include <thread>
+
 #include <iostream>
 #include <string>
+#include <cerrno>
+#include <cstring>
 
 #include "Socket.hpp"
 
@@ -25,23 +29,61 @@
     return local_address;
   }
 
+  void send_to(const sockaddr_in& remote_address,Socket& local_socket, int& perror){
+
+    std::string mensaje;
+    Message message{};
+
+    while(getline(std::cin,mensaje)){
+
+      mensaje.copy(message.text,sizeof(message)-1,0);
+      local_socket.Sent_to(message,remote_address,perror);
+
+      if(mensaje == "quit")
+        break;
+
+    }
+
+
+  }
+
+  void receive_from(sockaddr_in& remote_address,Socket& local_socket, int& perror){
+
+    Message message{};
+
+      while(1){
+
+        local_socket.Receive_from(message,remote_address,perror);
+
+        if(message.text == "quit")
+          break;
+          std::cout << message.remote_ip << " Envió: " << message.text << "\n";
+
+
+      }
+
+  }
+
 int main(){
 
 int proccess_error;
-std::string mensaje;
-Message texto_tosend;
-Message message;
-sockaddr_in remote_addr = make_ip_address("127.0.0.1",56666);
-Socket local_socket(make_ip_address("",57777),proccess_error);
 
-  while(getline(std::cin,mensaje)){
+try{
 
-    if(mensaje == "quit")
-      break;
+  sockaddr_in remote_addr = make_ip_address("192.168.1.72",57777);
+  Socket local_socket(make_ip_address("",56666),proccess_error);
 
-    mensaje.copy(texto_tosend.text,sizeof(texto_tosend)-1,0);
-    local_socket.Sent_to(texto_tosend,remote_addr,proccess_error);
-    local_socket.Receive_from(message,remote_addr);
+    std::thread send_thread(&send_to,std::ref(remote_addr),std::ref(local_socket),std::ref(proccess_error));
+    std::thread receive_thread(&receive_from,std::ref(remote_addr),std::ref(local_socket),std::ref(proccess_error));
 
-  }
+    send_thread.join();
+    receive_thread.join();
+
+}
+catch(std::system_error& e){
+
+    std::cerr << "MultiUser-Chat: " << e.what() << "\n";
+    return proccess_error;
+
+}
 }
--- Source/Socket.cpp
+++ Source/Socket.cpp
@@ -1,17 +1,20 @@
 #include "Socket.hpp"
 
-Socket::Socket(const sockaddr_in& address, int& serror){
+Socket::Socket(const sockaddr_in& address,int& serror){
 
    fd = socket(AF_INET, SOCK_DGRAM,0);
 
-  if(fd < 0)
+  if(fd < 0){
       serror = SOCKET_CREATION_ERROR;
-  else{
+      throw std::system_error(errno, std::system_category(),"no se pudo crear el Socket: ");
+  }else{
 
     int result = bind(fd,reinterpret_cast<const sockaddr*>(&address),sizeof(address));
 
-    if (result < 0)
+    if (result < 0){
       serror = SOCKET_BINDING_ERROR;
+      throw std::system_error(errno, std::system_category(), "No se pudo enlazar el socket: ");
+    }
   }
 }
 
@@ -24,19 +27,23 @@ Socket::~Socket(){
 void Socket::Sent_to(const Message& message, const sockaddr_in& remote_address, int& serror){
 
   int result = sendto(fd, &message, sizeof(message), 0, reinterpret_cast<const sockaddr*>(&remote_address),sizeof(remote_address));
-  if(result < 0)
+  if(result < 0){
     serror = ERROR_SENDING_MESSAGE;
+    throw std::system_error(errno, std::system_category(), "No se pudo enviar el mensaje: ");
+  }
 }
 
-void Socket::Receive_from(Message& message, sockaddr_in& remote_address){
+void Socket::Receive_from(Message& message, sockaddr_in& remote_address, int& serror){
 
   socklen_t src_len = sizeof(remote_address);
 
   int result = recvfrom(fd, &message, message.size,0,reinterpret_cast<sockaddr*>(&remote_address),&src_len);
+    if(result < 0){
+      serror = ERROR_RECEIVING_MESSAGE;
+      throw std::system_error(errno, std::system_category(), "No se pudo recibir el mensaje: ");
+    }
 
-  char* remote_ip = inet_ntoa(remote_address.sin_addr);
-  int remote_port = ntohs(remote_address.sin_port);
+  message.remote_ip = inet_ntoa(remote_address.sin_addr);
+  message.remote_port = ntohs(remote_address.sin_port);
 
-  message.text[254] = '\0';
-  std::cout << "El sistema " << remote_ip << ":" << remote_port << "envia el mensaje: " << message.text << "\n";
 }
